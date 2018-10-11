0a9fe79357509ff5dae5e6ad49fed087e7a176b3
Hadi <email@example.com>
2018-10-11 15:15:32 +0100

    Testing moving tag

--- fyp/src/main.py
+++ fyp/src/main.py
@@ -24,9 +24,9 @@ def talker():
     # Inifinite loop
     while not rospy.is_shutdown():
         # Pubblish a dummy data for now, this will publish the route that we are going to need
-        pub_RouteN.publish("3")
+        pub_RouteN.publish("1")
         # Show me the route
-        rospy.loginfo("Route 3")
+        rospy.loginfo("Route 1")
         # Sleep for 10hz
         rate.sleep()
 
