425f48368136b4af9bbf4bb8541c7fa0d08047f6
20Chix <hadi20107@hotmail.co.uk>
2019-02-11 22:40:30 +0000

    Added battery level in ROS

--- fyp/src/drone_batteryLevelRaspberryPi.py
+++ fyp/src/drone_batteryLevelRaspberryPi.py
@@ -14,25 +14,72 @@
 #float getsoc();
 #
 
+
+
+
+import struct
+import smbus
+import sys
+import time
 import os
 import rospy
-# import Raspi UPS Hat library
-import raspiupshat
+
+state_charge = (3.622, 3.832, 4.043, 4.182, 4.21)
+state_discharge = (4.17, 3.74751, 3.501, 3.35, 2.756)
+state_charging = False;
+v_current = 0;
+v_old = 0;
+capacity = 0;
+publisherAidBatteryLevelRaspberryPi = rospy.Publisher('aid/batteryLevelRaspberryPi', int, queue_size=10)
+publisherAidVoltageLevelRaspberryPi = rospy.Publisher('aid/voltageLevelRaspberryPi', int, queue_size=10)
+rate = rospy.Rate(10)  # 10hz
+
+def readVoltage(bus):
+
+    "This function returns as float the voltage from the Raspi UPS Hat via the provided SMBus object"
+    address = 0x36
+    read = bus.read_word_data(address, 2)
+    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+    voltage = swapped * 78.125 / 1000000
+    return voltage
 
 
-publisherAidBatteryLevelRaspberryPi  = rospy.Publisher('aid/batteryLevelRaspberryPi', int, queue_size=10)
-publisherAidVoltageLevelRaspberryPi  = rospy.Publisher('aid/voltageLevelRaspberryPi', int, queue_size=10)
+def readCapacity(bus):
+    "This function returns as a float the remaining capacity of the battery connected to the Raspi UPS Hat via the provided SMBus object"
+    address = 0x36
+    read = bus.read_word_data(address, 4)
+    swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+    capacity = swapped / 256
+    return capacity
 
-# init Raspi UPS Hat
-raspiupshat.init();
 
-rate = rospy.Rate(10) # 10hz
+bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+v_old = readVoltage(bus)
+v_current = v_old
+
+
+
+if (v_current > state_charge[4]):
+    capacity = 100
+elif ((v_current < state_charge[4]) and (v_current >= state_charge[3])):
+    capacity = (v_current - state_charge[3]) / ((state_charge[4] - state_charge[3]) / 25) + 75
+elif ((v_current < state_charge[3]) and (v_current >= state_charge[2])):
+    capacity = (v_current - state_charge[2]) / ((state_charge[3] - state_charge[2]) / 25) + 50
+elif ((v_current < state_charge[2]) and (v_current >= state_charge[1])):
+    capacity = (v_current - state_charge[1]) / ((state_charge[2] - state_charge[1]) / 25) + 25
+elif ((v_current < state_charge[1]) and (v_current >= state_charge[0])):
+    capacity = (v_current - state_charge[0]) / ((state_charge[1] - state_charge[0]) / 25)
+else:
+    capacity = 0
 
 while not rospy.is_shutdown():
 
     # Get info
-    rospy.loginfo("Voltage:%5.2fV" % raspiupshat.getv())
-    rospy.loginfo("Battery:%5i%%" % raspiupshat.getsoc())
+    rospy.loginfo("Voltage:%5.2fV" % readVoltage(bus))
+    rospy.loginfo("Battery:%5i%%" % capacity)
 
     publisherAidBatteryLevelRaspberryPi.publish(raspiupshat.getsoc())
     publisherAidVoltageLevelRaspberryPi.publish(raspiupshat.getv())
@@ -47,3 +94,4 @@ while not rospy.is_shutdown():
             os.system("sudo shutdown")
 
     rate.sleep()
+
