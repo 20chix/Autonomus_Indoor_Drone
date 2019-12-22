df80dc3a93d8f89a580cfecfdfb015b4a9715ac7
20chix <hadi20107@hotmail.co.uk>
2019-12-22 17:37:27 +0000

    Added ardupilot sdf files

--- launch_sim.sh
+++ launch_sim.sh
@@ -1,5 +1,5 @@
 # Launch ardupilot simulation only gazebo world
-gazebo --verbose ~/catkin_ws/src/ardupilot_gazebo/worlds/iris_arducopter_runway.world
+gazebo --verbose ~/catkin_ws/src/ardupilot_gazebo/worlds/iris_ardupilot.world
 
 # Launch sim vehicle so ros topics can appear on terminal
 /home/hadi/ardupilot/Tools/autotest/sim_vehicle.py --map --console  -v ArduCopter -f gazebo-iris
